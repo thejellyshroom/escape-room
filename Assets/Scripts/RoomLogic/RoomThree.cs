09d9b482daae4d7a87873411832c7479a91ca89a
jellyshroom <thejellyshroom@gmail.com>
2025-03-09 22:23:24 -0700

    text ui

--- Assets/Scripts/RoomLogic/RoomThree.cs
+++ Assets/Scripts/RoomLogic/RoomThree.cs
@@ -55,7 +55,7 @@ public class RoomThree : MonoBehaviour
             book.SetActive(false);
             if (textManager != null)
             {
-                textManager.UpdateText("Brown book selected! Click on the empty bookshelf to place it.");
+                textManager.UpdateText("Brown book selected!");
             }
             selectedBook = brownBook;
         }
@@ -64,7 +64,7 @@ public class RoomThree : MonoBehaviour
             book.SetActive(false);
             if (textManager != null)
             {
-                textManager.UpdateText("Green book selected! Click on the empty bookshelf to place it.");
+                textManager.UpdateText("Green book selected!");
             }
             selectedBook = greenBook;
         }
@@ -73,7 +73,7 @@ public class RoomThree : MonoBehaviour
             book.SetActive(false);
             if (textManager != null)
             {
-                textManager.UpdateText("Red book selected! Click on the empty bookshelf to place it.");
+                textManager.UpdateText("Red book selected!");
             }
             selectedBook = redBook;
         }
